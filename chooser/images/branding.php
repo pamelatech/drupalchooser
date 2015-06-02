65b1edb578021a7f7f153f2a781ee7538cc575cd
pamelatech <pamelatech@yahoo.com>
2015-06-02 20:49:35 +0000

    working copy for non-federated resources

---
+++ chooser/images/branding.php
@@ -0,0 +1,12 @@
+<?php
+header('Content-Type: text/html');
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Max-Age: 86400');
+header('Access-Control-Allow-Credentials: true');
+?>
+<html>
+  <body>
+    <p>hello there</p>
+  </body>
+</html>
